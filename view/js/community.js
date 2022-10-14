89b0344541f82f44eb447b3ea2bc98e1d2298a9e
gitPiyushhh <Piyushhhagarwal@gmail.com>
2022-10-15 02:55:02 +0530

    frontend stack complete

--- view/js/community.js
+++ view/js/community.js
@@ -22,34 +22,128 @@ homeNav.addEventListener("click", (e) => {
   e.preventDefault();
   menubar__line.classList.toggle("u-hide-nav-line");
   navbar_mobile.classList.toggle("u-hidden");
-  location.assign('index.html')
+  location.assign("index.html");
 });
 
 projectNav.addEventListener("click", (e) => {
   e.preventDefault();
   menubar__line.classList.toggle("u-hide-nav-line");
   navbar_mobile.classList.toggle("u-hidden");
-  location.assign('index.html#projects')
+  location.assign("index.html#projects");
 });
 
 certNav.addEventListener("click", (e) => {
   e.preventDefault();
   menubar__line.classList.toggle("u-hide-nav-line");
   navbar_mobile.classList.toggle("u-hidden");
-  
-  location.assign('index.html#certificates')
+
+  location.assign("index.html#certificates");
 });
 
 communityNav.addEventListener("click", (e) => {
   e.preventDefault();
   menubar__line.classList.toggle("u-hide-nav-line");
   navbar_mobile.classList.toggle("u-hidden");
-  location.assign('#')
+  location.assign("#");
 });
 
 contactNav.addEventListener("click", (e) => {
   e.preventDefault();
   menubar__line.classList.toggle("u-hide-nav-line");
   navbar_mobile.classList.toggle("u-hidden");
-  location.assign('index.html#contact')
+  location.assign("index.html#contact");
 });
+
+const snippetBack1 = document.getElementById("snippet-1back");
+const snippetFwd1 = document.getElementById("snippet-1fwd");
+const snippetBack2 = document.getElementById("snippet-2back");
+const snippetFwd2 = document.getElementById("snippet-2fwd");
+const snippetBack3 = document.getElementById("snippet-3back");
+const snippetFwd3 = document.getElementById("snippet-3fwd");
+const test1 = document.getElementById("test1");
+const test2 = document.getElementById("test2");
+const test3 = document.getElementById("test3");
+
+
+const fwd = (e, i, test, cnt) => {
+  e.preventDefault();
+  console.log(i)
+  test.innerHTML = `<img src="./drawables/frontend - ${i} --${cnt}.png" alt="Snippet 1" class="community-main__snippets-img" id="test"></img>`;
+}
+
+// Globals
+i = 1;
+snippetFwd1.addEventListener('click', function(e) {
+
+  if (i == 9) {
+    // ROllback
+    i = 0;
+  }
+
+  e.preventDefault();
+  ++i;
+  fwd(e, 1, test1, i)
+})
+
+snippetBack1.addEventListener('click', function(e) {
+
+  if (i == 0 || i-1 == 0) {
+    // ROllback
+    alert('No page behind 🙃');
+    return;
+  }
+
+  e.preventDefault();
+  --i;
+  fwd(e, 1, test1, i)
+})
+
+snippetFwd2.addEventListener('click', function(e) {
+
+  if (i == 9) {
+    // ROllback
+    i = 0;
+  }
+
+  e.preventDefault();
+  ++i;
+  fwd(e, 2, test2, i)
+})
+
+snippetBack2.addEventListener('click', function(e) {
+
+  if (i == 0 || i-1 == 0) {
+    // ROllback
+    alert('No page behind 🙃');
+    return;
+  }
+
+  e.preventDefault();
+  --i;
+  fwd(e, 2, test2, i)
+})
+
+snippetFwd3.addEventListener('click', function(e) {
+
+  if (i == 8) {
+    // ROllback
+    i = 0;
+  }
+
+  e.preventDefault();
+  ++i;
+  fwd(e, 3, test3, i)
+})
+
+snippetBack3.addEventListener('click', function(e) {
+
+  if (i == 0 || i-1 == 0) {
+    // ROllback
+    alert('No page behind 🙃');
+    return;
+  }
+
+  e.preventDefault();
+  --i;
+  fwd(e, 3, test3, i)
+})
